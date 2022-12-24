83e3f553c73988dec3fdad7401be0484467c1e8f
Evgeniy <evgen-z2007@yandex.ru>
2022-12-24 23:33:23 +0500

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Task2 task2 = new Task2();
+        task2.result(new int[] {1, 2, 3, 4}, 3, 2);
+
+        Task3 task3 = new Task3();
+        task3.result();
+    }
+
+}
